3731dbae3d58265670047cb8acdc4df886e1792e
NAJEEBKHAN123 <najeebkhanlaku@gmail.com>
2025-03-18 22:55:09 +0500

    add signin page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import Signup from './pages/Signup'
 import Signin from './pages/Signin'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
 
 
 function App() {
   return (
-    <div>
-      <Signup/>
-      <Signin/>
-    </div>
+
+    <BrowserRouter>
+     
+     <Routes>
+      {/* <Route path='/' element={<Home/>}/> */}
+      <Route path='/' element={<Signup/>}/>
+      <Route path='/login' element={<Signin/>}/>
+     </Routes>
+    </BrowserRouter>
+
   )
 }
 
